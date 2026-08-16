420a119dc534ef21092d73e0922ed1c9b6340320
Alex Ciobanas <aciobanas@live.com>
2026-08-16 16:39:22 +0000

    Format

--- gtherm-units/src/isq/si_quantities.rs
+++ gtherm-units/src/isq/si_quantities.rs
@@ -8,107 +8,152 @@
 
 use crate::qcharacter::QCharacter;
 use crate::qspec::{QSpec, QSpecEq};
+use QSpecEq::{Div, Mul, Pow, Term};
 
 use crate::isq::Isq;
 
 impl Isq {
     // space and time
 
-    pub const WIDTH: QSpec = QSpec::new("width").equation(&QSpecEq::Term(&Isq::L));
+    pub const WIDTH: QSpec = QSpec::new("width").equation(&Term(&Isq::L));
+
     pub const BREADTH: QSpec = Isq::WIDTH;
+
     /// Differs from ISO 80000: a child of `width`, not of `length` directly.
-    pub const RADIUS: QSpec = QSpec::new("radius").equation(&QSpecEq::Term(&Isq::WIDTH));
-    pub const PATH_LENGTH: QSpec = QSpec::new("path_length").equation(&QSpecEq::Term(&Isq::L));
+    pub const RADIUS: QSpec = QSpec::new("radius").equation(&Term(&Isq::WIDTH));
+
+    pub const PATH_LENGTH: QSpec = QSpec::new("path_length").equation(&Term(&Isq::L));
+
     pub const ARC_LENGTH: QSpec = Isq::PATH_LENGTH;
-    pub const AREA: QSpec = QSpec::new("area").equation(&QSpecEq::Pow(&QSpecEq::Term(&Isq::L), 2));
-    pub const ANGULAR_MEASURE: QSpec = QSpec::new("angular_measure").equation(&QSpecEq::Div(
-        &QSpecEq::Term(&Isq::ARC_LENGTH),
-        &QSpecEq::Term(&Isq::RADIUS),
-    ));
-    pub const SOLID_ANGULAR_MEASURE: QSpec = QSpec::new("solid_angular_measure").equation(&QSpecEq::Div(
-        &QSpecEq::Term(&Isq::AREA),
-        &QSpecEq::Pow(&QSpecEq::Term(&Isq::RADIUS), 2),
-    ));
-    pub const PERIOD_DURATION: QSpec = QSpec::new("period_duration").equation(&QSpecEq::Term(&Isq::T));
+
+    pub const AREA: QSpec = QSpec::new("area").equation(&Pow(&Term(&Isq::L), 2));
+
+    pub const ANGULAR_MEASURE: QSpec = QSpec::new("angular_measure").equation(
+        &Div(
+            &Term(&Isq::ARC_LENGTH),
+            &Term(&Isq::RADIUS),
+        )
+    );
+
+    pub const SOLID_ANGULAR_MEASURE: QSpec = QSpec::new("solid_angular_measure").equation(
+        &Div(
+            &Term(&Isq::AREA),
+            &Pow(&Term(&Isq::RADIUS), 2),
+        )
+    );
+
+    pub const PERIOD_DURATION: QSpec = QSpec::new("period_duration").equation(&Term(&Isq::T));
+
     pub const PERIOD: QSpec = Isq::PERIOD_DURATION;
+
     pub const FREQUENCY: QSpec =
-        QSpec::new("frequency").equation(&QSpecEq::Pow(&QSpecEq::Term(&Isq::PERIOD_DURATION), -1));
+        QSpec::new("frequency").equation(&Pow(&Term(&Isq::PERIOD_DURATION), -1));
 
     // mechanics
 
     /// Differs from ISO 80000.
-    pub const ENERGY: QSpec = QSpec::new("energy").equation(&QSpecEq::Div(
-        &QSpecEq::Mul(&QSpecEq::Term(&Isq::M), &QSpecEq::Pow(&QSpecEq::Term(&Isq::L), 2)),
-        &QSpecEq::Pow(&QSpecEq::Term(&Isq::T), 2),
-    ));
+    pub const ENERGY: QSpec = QSpec::new("energy").equation(
+        &Div(
+            &Mul(&Term(&Isq::M), &Pow(&Term(&Isq::L), 2)),
+            &Pow(&Term(&Isq::T), 2),
+        )
+    );
+
     pub const FORCE: QSpec = QSpec::new("force")
-        .equation(&QSpecEq::Div(
-            &QSpecEq::Mul(&QSpecEq::Term(&Isq::M), &QSpecEq::Term(&Isq::L)),
-            &QSpecEq::Pow(&QSpecEq::Term(&Isq::T), 2),
-        ))
+        .equation(
+            &Div(
+                &Mul(&Term(&Isq::M), &Term(&Isq::L)),
+                &Pow(&Term(&Isq::T), 2),
+            )
+        )
         .character(QCharacter::REAL_VECTOR);
+
     pub const PRESSURE: QSpec = QSpec::new("pressure")
-        .equation(&QSpecEq::Div(&QSpecEq::Term(&Isq::FORCE), &QSpecEq::Term(&Isq::AREA)))
-        .character(QCharacter::REAL_SCALAR);
+        .equation(&Div(&Term(&Isq::FORCE), &Term(&Isq::AREA)));
 
     // electromagnetism
 
-    pub const ELECTRIC_POTENTIAL: QSpec = QSpec::new("electric_potential")
-        .equation(&QSpecEq::Div(
-            &QSpecEq::Term(&Isq::ENERGY),
-            &QSpecEq::Mul(&QSpecEq::Term(&Isq::I), &QSpecEq::Term(&Isq::T)),
-        ))
-        .character(QCharacter::REAL_SCALAR);
-    pub const CAPACITANCE: QSpec = QSpec::new("capacitance").equation(&QSpecEq::Div(
-        &QSpecEq::Mul(&QSpecEq::Term(&Isq::I), &QSpecEq::Term(&Isq::T)),
-        &QSpecEq::Term(&Isq::ELECTRIC_POTENTIAL),
-    ));
+    pub const ELECTRIC_POTENTIAL: QSpec = QSpec::new("electric_potential").equation(
+        &Div(
+            &Term(&Isq::ENERGY),
+            &Mul(&Term(&Isq::I), &Term(&Isq::T)),
+        )
+    );
+
+    pub const CAPACITANCE: QSpec = QSpec::new("capacitance").equation(
+        &Div(
+            &Mul(&Term(&Isq::I), &Term(&Isq::T)),
+            &Term(&Isq::ELECTRIC_POTENTIAL),
+        )
+    );
+
     pub const IMPEDANCE: QSpec = QSpec::new("impedance")
-        .equation(&QSpecEq::Div(&QSpecEq::Term(&Isq::ELECTRIC_POTENTIAL), &QSpecEq::Term(&Isq::I)))
+        .equation(
+            &Div(
+                &Term(&Isq::ELECTRIC_POTENTIAL),
+                &Term(&Isq::I)
+            )
+        )
         .character(QCharacter::COMPLEX_SCALAR);
+
     pub const ADMITTANCE: QSpec = QSpec::new("admittance")
-        .equation(&QSpecEq::Pow(&QSpecEq::Term(&Isq::IMPEDANCE), -1))
+        .equation(&Pow(&Term(&Isq::IMPEDANCE), -1))
         .character(QCharacter::COMPLEX_SCALAR);
+
     pub const MAGNETIC_FLUX_DENSITY: QSpec = QSpec::new("magnetic_flux_density")
-        .equation(&QSpecEq::Div(
-            &QSpecEq::Term(&Isq::M),
-            &QSpecEq::Mul(&QSpecEq::Term(&Isq::I), &QSpecEq::Pow(&QSpecEq::Term(&Isq::T), 2)),
+        .equation(&Div(
+            &Term(&Isq::M),
+            &Mul(&Term(&Isq::I), &Pow(&Term(&Isq::T), 2)),
         ))
         .character(QCharacter::REAL_VECTOR);
 
     // light_and_radiation
 
-    pub const LUMINOUS_FLUX: QSpec = QSpec::new("luminous_flux").equation(&QSpecEq::Mul(
-        &QSpecEq::Term(&Isq::J),
-        &QSpecEq::Term(&Isq::SOLID_ANGULAR_MEASURE),
-    ));
+    pub const LUMINOUS_FLUX: QSpec = QSpec::new("luminous_flux").equation(
+        &Mul(
+            &Term(&Isq::J),
+            &Term(&Isq::SOLID_ANGULAR_MEASURE),
+        )
+    );
+
     pub const ILLUMINANCE: QSpec = QSpec::new("illuminance")
-        .equation(&QSpecEq::Div(&QSpecEq::Term(&Isq::LUMINOUS_FLUX), &QSpecEq::Term(&Isq::AREA)));
+        .equation(&Div(&Term(&Isq::LUMINOUS_FLUX), &Term(&Isq::AREA)));
 
     // physical_chemistry
 
     pub const CATALYTIC_ACTIVITY: QSpec = QSpec::new("catalytic_activity")
-        .equation(&QSpecEq::Div(&QSpecEq::Term(&Isq::N), &QSpecEq::Term(&Isq::T)));
+        .equation(&Div(&Term(&Isq::N), &Term(&Isq::T)));
 
     // atomic_and_nuclear_physics
 
-    pub const ACTIVITY: QSpec = QSpec::new("activity").equation(&QSpecEq::Pow(&QSpecEq::Term(&Isq::T), -1));
+    pub const ACTIVITY: QSpec = QSpec::new("activity").equation(&Pow(&Term(&Isq::T), -1));
+
     pub const ABSORBED_DOSE: QSpec = QSpec::new("absorbed_dose")
-        .equation(&QSpecEq::Div(&QSpecEq::Term(&Isq::ENERGY), &QSpecEq::Term(&Isq::M)));
+        .equation(&Div(&Term(&Isq::ENERGY), &Term(&Isq::M)));
+
     pub const IONIZING_RADIATION_QUALITY_FACTOR: QSpec = QSpec::new("ionizing_radiation_quality_factor");
-    pub const DOSE_EQUIVALENT: QSpec = QSpec::new("dose_equivalent").equation(&QSpecEq::Mul(
-        &QSpecEq::Term(&Isq::ABSORBED_DOSE),
-        &QSpecEq::Term(&Isq::IONIZING_RADIATION_QUALITY_FACTOR),
-    ));
+
+    pub const DOSE_EQUIVALENT: QSpec = QSpec::new("dose_equivalent").equation(
+        &Mul(
+            &Term(&Isq::ABSORBED_DOSE),
+            &Term(&Isq::IONIZING_RADIATION_QUALITY_FACTOR),
+        )
+    );
 
     // not in ISO 80000 but central for mechanics, thermodynamics and chemistry
 
-    pub const POWER: QSpec = QSpec::new("power").equation(&QSpecEq::Div(
-        &QSpecEq::Mul(&QSpecEq::Term(&Isq::M), &QSpecEq::Pow(&QSpecEq::Term(&Isq::L), 2)),
-        &QSpecEq::Pow(&QSpecEq::Term(&Isq::T), 3),
-    ));
-    pub const ENERGY_DENSITY: QSpec = QSpec::new("energy_density").equation(&QSpecEq::Div(
-        &QSpecEq::Term(&Isq::M),
-        &QSpecEq::Mul(&QSpecEq::Term(&Isq::L), &QSpecEq::Pow(&QSpecEq::Term(&Isq::T), 2)),
-    ));
+    pub const POWER: QSpec = QSpec::new("power").equation(
+        &Div(
+            &Mul(&Term(&Isq::M), &Pow(&Term(&Isq::L), 2)),
+            &Pow(&Term(&Isq::T), 3),
+        )
+    );
+
+    pub const ENERGY_DENSITY: QSpec = QSpec::new("energy_density").equation(
+        &Div(
+            &Term(&Isq::M),
+            &Mul(&Term(&Isq::L), &Pow(&Term(&Isq::T), 2)),
+        )
+    );
+
 }
